4e98a56f5e2c57b49222df1064891f932af75393
dwang657 <157731102+dwang657@users.noreply.github.com>
2026-09-22 11:43:39 -0400

    Add: valid-sudoku - submission-5

---
+++ Data Structures & Algorithms/valid-sudoku/submission-5.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # dictionaries mapping row/col indices to lists
+        rows = defaultdict(list)
+        cols = defaultdict(list)
+        # dictionary to map tuples to lists for each square
+        # the tuples act like coordinates of each square
+        squares = defaultdict(list)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+
+                if (board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]):
+                    return False
+                
+                rows[r].append(board[r][c])
+                cols[c].append(board[r][c])
+                squares[(r // 3, c // 3)].append(board[r][c])
+
+        return True
+
+
+
+
+
+
+
+
+
